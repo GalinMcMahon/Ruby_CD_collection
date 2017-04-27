719b02219b66c704da05085112f833e0d7bca483
Galin McMahon and Jin Camou <galin@>
2017-04-27 12:40:56 -0700

    route from index to artist list

--- app.rb
+++ app.rb
@@ -1,10 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require './lib/albums'
+require './lib/artists'
 require 'pry'
 
 also_reload('lib/**/*.rb')
 
 get('/') do
   erb(:index)
+end
+
+# post('new_artist') do
+#   erb(:artists)
+# end
+
+get('/artists') do
+  @artists = Artists.all()
+  erb(:artists)
 end
